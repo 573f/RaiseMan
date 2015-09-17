b7a1d87d762a4e8a94c1f4aae410429c3683ec36
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-17 14:01:06 -0400

    Chapter 9: RaiseMan lives!

---
+++ RaiseMan/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Stephen Skubik-Peplaski on 9/17/15.
+//  Copyright © 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
