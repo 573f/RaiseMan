0a783fd968296a25a8a4282546db0a2d1aef3e31
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-17 12:41:18 -0400

    Initial Commit

---
+++ RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by Stephen Skubik-Peplaski on 9/17/15.
+//  Copyright © 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
